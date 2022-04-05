add1f98e9530145efdec08d5f2e24f06dd7b0491
azubkov93 <46124676+azubkov93@users.noreply.github.com>
2022-04-05 18:05:16 +0300

    Exception

--- test_main_page.py
+++ test_main_page.py
@@ -24,8 +24,14 @@ from .pages.main_page import MainPage
 # page.add_product_to_cart()            # жмем кнопку добавить в корзину
 # page.should_be_success_message()      # проверяем что есть сообщение с нужным текстом
 
-def test_guest_can_go_to_login_page(browser):
+# def test_guest_can_go_to_login_page(browser):
+#     link = "http://selenium1py.pythonanywhere.com/"
+#     page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+#     page.open()  # открываем страницу
+#     page.go_to_login_page()  # выполняем метод страницы — переходим на страницу логина
+
+def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()  # открываем страницу
-    page.go_to_login_page()  # выполняем метод страницы — переходим на страницу логина
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
